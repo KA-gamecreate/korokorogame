8b25e7ff9e7ffeef7a08f2875ee94809f1fbdcdf
”KA-gamecreate” <burakki.efi.22@docomo.ne.jp>
2020-06-14 18:21:02 +0900

    “最後まで終わった”

---
+++ Assets/FadeEffect/Script/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    public float power = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -10)
+        {
+            FadeManager.Instance.LoadLevel("GameOver",2.0f);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(0, 0, power);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(0,0,-power);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(-power,0, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(power,0, 0);
+        }
+
+        
+    }
+}
